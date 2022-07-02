db6b4138dbac5506aa29cafd888fd74f814fff70
Helder Sérvio <heldersrvio@gmail.com>
2022-07-02 14:21:16 -0300

    FEAT: ADD INTEGRATION TO AWS LAMBDA

--- src/main/java/com/heldersrvio/springbootlizards/security/WebServiceConfig.java
+++ src/main/java/com/heldersrvio/springbootlizards/security/WebServiceConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -16,5 +17,6 @@ public class WebServiceConfig extends WebSecurityConfigurerAdapter {
                 .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
